ce506de5e60b85418fd70e00f9c5029abc4ad833
Louismoynebressand <114920229+Louismoynebressand@users.noreply.github.com>
2023-03-28 13:36:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const divGauche = document.querySelector('.gauche');
+
+
+
+
+
+
+if (divGauche !== null) {
+    divGauche.addEventListener('click', function() {
+      window.location.href = 'page_cv.html';
+    });
+  }
